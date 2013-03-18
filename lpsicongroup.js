7f3d5fc6ce77b97b66d5fcfe56265c91f450d3b8
JungHyun Kim <kjunghyun+github@gmail.com>
2013-03-18 21:52:11 +0900

    Move Delay 0

--- lpsicongroup.js
+++ lpsicongroup.js
@@ -25,7 +25,7 @@ function makeLaunchpad(args){
     $(container).sortable({
 	    items: item,
 	    cursor: "move",
-	    delay: 100,
+	    // delay: 100,
 	    forcePlaceholderSize: false,
 	    out: function(event, ui){
 	    },
